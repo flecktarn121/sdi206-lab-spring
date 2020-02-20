c90e1888aa2975d27d56bc523e5b5dc0be688ae3
Ángel García Menéndez <UO258654@uniovi.es>
2020-02-20 10:54:51 +0100

    SDI–session3-Ejercicio-complementario3 (sin terminar)

---
+++ src/main/java/com/uniovi/validators/MarkValidator.java
@@ -0,0 +1,32 @@
+package com.uniovi.validators;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.uniovi.entities.Mark;
+
+@Controller
+public class MarkValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return Mark.class.equals(aClass);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Mark mark = (Mark) target;
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "score", "Error.empty");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "Error.empty");
+		if (mark.getScore() < 0 || mark.getScore()> 10) {
+			errors.rejectValue("score", "Error.addmark.score.length");
+		}
+		
+		if(mark.getDescription().length() < 20){
+			errors.rejectValue("description", "Error.addmark.description.length");
+		}
+
+	}
+}
